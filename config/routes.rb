e2bfb04df0be81f347acdd9828ad87f27ea08c47
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-03-09 17:13:04 +0100

    les routes sont done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :profile, only: [:show]
+
+  resources :restaurants, only: [:index, :show]
+
+  resources :orders, only: [:index, :show, :create]
 end
