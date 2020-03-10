a38e1407103699aa907278b41fd8a32f9ac481fc
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-03-10 17:16:05 +0100

    fix bug model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,5 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :restaurant_id, presence: true, uniqueness: true
+  validates :last_name,  presence: true
 end
