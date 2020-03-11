5889041c9ad7b56c75833a9c39cab18d890cccd7
Justine Barbauk <justinehsd@gmail.com>
2020-03-11 15:22:24 +0100

    card and footer and index done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,16 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+
+    @restaurants = Restaurant.geocoded #returns flats with coordinates
+    @markers = @restaurants.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude
+      }
+    end
   end
+
 
   def show
   end
