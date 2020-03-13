111fd0120ed27f03544391bad48336f0f5587b93
Justine Barbauk <justinehsd@gmail.com>
2020-03-13 11:21:38 +0100

    push routes de malade, back de ouf, flow is DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'orders/opened', to: 'pages#orders_opened'
+  get 'meals/recolted', to: 'pages#meals_recolted'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :profile, only: [:show]
 
   resources :restaurants, only: [:index, :show]
 
   resources :orders, only: [:index, :show, :create]
+
+  resource :meal, only: [] do
+    patch :release
+  end
 end
