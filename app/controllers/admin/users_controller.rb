0d0a79078e58d178d6bfa2d9b7af114270c9e920
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-04-21 17:30:39 +0200

    controller admin menus & users done

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,18 +1,42 @@
 class Admin::UsersController < ApplicationController
 
   def index
+    @my_resto_users = current_user.restaurant.users
+    @users = @my_resto_users.where.not(id: current_user.id)
+  end
 
+  def new
+    @user = User.new
   end
 
   def edit
-
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "L'employé a bien été créé."
+    else
+      render :new
+    end
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "L'employé a bien été modifié."
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "L'employé a bien été supprimé."
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :admin)
   end
 end
