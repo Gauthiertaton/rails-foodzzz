4b3810ab85986cca1b81771e88faaf83a2265c9a
marion henrio <marion.henrio@hotmail.fr>
2020-06-08 20:57:09 +0200

    navbar_issue_solved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
-import { classToggle } from '../plugins/navbar_menu';
+// import { classToggle } from '../plugins/navbar_menu';
 import '../plugins/show_user_index';
 // import { myFunction } from '../plugins/show_user_index';
 initMapbox();
-classToggle();
+// classToggle();
 
